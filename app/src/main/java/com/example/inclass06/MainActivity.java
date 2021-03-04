5387e40533ca0b9f913bc73f9fb4f3ca5f75f597
Komal Patel <kpate201@uncc.edu>
2021-03-04 18:08:04 -0500

    thread chnages final

--- app/src/main/java/com/example/inclass06/MainActivity.java
+++ app/src/main/java/com/example/inclass06/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
@@ -16,6 +16,8 @@ import java.util.concurrent.ExecutorService;
 import android.os.AsyncTask;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,9 +27,7 @@ public class MainActivity extends AppCompatActivity {
     ExecutorService threadPool;
     Handler handler;
     ProgressBar progressBar;
-    String TAG = "demo";
     SeekBar seekBar;
-    TextView p;
     ArrayList<Double> threadArray = new ArrayList<>(20);
     ArrayAdapter threadAdapter;
     ListView listviewresult;
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<Double> resultList;
     int arraySize;
     Double average = 0.0;
+    Double sum = 0.0;
 
     public int selectedProgress = 0;
 
@@ -48,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
         threadButton = findViewById(R.id.btnThread);
         progressBar = findViewById(R.id.progressBar);seekBar = findViewById(R.id.seekBar);
         txtprogress = findViewById(R.id.textView_complexity);
-        p = findViewById(R.id.tvProgress);
         progressBar.setVisibility(View.GONE);
         txtprogresshappened = findViewById(R.id.tvProgress);
         txtaverage = findViewById(R.id.textView_average);
@@ -59,53 +59,27 @@ public class MainActivity extends AppCompatActivity {
         btn_asynchtask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                result.clear();
+                threadArray.clear();
                 new DoWorkAsynch().execute(selectedProgress);
                 btn_asynchtask.setEnabled(false);
                 threadButton.setEnabled(false);
+                seekBar.setEnabled(false);
             }
         });
-
         seekBar.setMax(20);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress,
                                           boolean fromUser) {
-                threadArray.clear();
                 txtprogress.setText(progress+" "+getResources().getString(R.string.seekbar_count));
-
                 selectedProgress = progress;
-                Log.d(TAG, "onProgressChanged: "+selectedProgress);
-
-                for(int i=0;i<selectedProgress;i++){
-                    threadPool.execute(new DoWorkThread());
+                if(progress == 0){
+                    result.clear();
+                    threadArray.clear();
+                    Toast.makeText(MainActivity.this, getResources().getString(R.string.select_complexity), Toast.LENGTH_SHORT).show();
                 }
-
-                handler = new Handler(new Handler.Callback() {
-                    @Override
-                    public boolean handleMessage(@NonNull Message msg) {
-                        progressBar.setProgress(0);
-                        progressBar.setMax(20);
-                        progressBar.setProgress(progressBar.getProgress()+1);
-                        threadArray.add((Double) msg.obj);
-                        Double sum = 0.0;
-                        int size = threadArray.size();
-                        arraySize = size;
-                        sum = ((Double) msg.obj).doubleValue()+sum;
-                        Double average = sum / (size);
-                        Log.d(TAG, "handleMessage: "+arraySize+threadArray);
-
-                        if(!threadButton.isEnabled()){
-                            threadAdapter = new ArrayAdapter<Double>(MainActivity.this, android.R.layout.simple_list_item_1,
-                                    android.R.id.text1,threadArray);
-                            listviewresult.setAdapter(threadAdapter);
-                            threadAdapter.notifyDataSetChanged();
-//                                    txtaverage.setText(average+"");
-                        }
-                        return false;
-                    }
-                });
-
             }
 
             @Override
@@ -118,46 +92,124 @@ public class MainActivity extends AppCompatActivity {
                 btn_asynchtask.setEnabled(true);
             }
         });
+
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(@NonNull Message msg) {
+
+                switch (msg.what){
+                    case DoWorkThread.STATUS_START:
+                        progressBar.setProgress(0);
+                        int progress = msg.getData().getInt(DoWorkThread.PROGRESS_KEY);
+                        progressBar.setMax(selectedProgress);
+                        break;
+
+                    case DoWorkThread.STATUS_PROGRESS:
+
+                        Double number = msg.getData().getDouble(DoWorkThread.NUMBER_KEY);
+                        if(threadArray.size()<selectedProgress) {
+                            threadArray.add(number);
+                            progressBar.setProgress(threadArray.size());
+                        }
+
+                        for (double element: threadArray) {
+                            threadAdapter = new ArrayAdapter<Double>(MainActivity.this, android.R.layout.simple_list_item_1,
+                                    android.R.id.text1,threadArray);
+                            listviewresult.setAdapter(threadAdapter);
+                            threadAdapter.notifyDataSetChanged();
+                        }
+                        int size = threadArray.size();
+                        if(size<=selectedProgress) {
+                            arraySize = size;
+                            sum = sum + number ;
+                            Double average = sum / (size);
+                            txtprogresshappened.setText(arraySize+getResources().getString(R.string.slash)+selectedProgress);
+
+                            txtaverage.setText(getResources().getString(R.string.average)+" "+average);
+                        }
+                        break;
+
+                    case DoWorkThread.STATUS_END:
+                        threadButton.setEnabled(true);
+                        seekBar.setEnabled(true);
+                        btn_asynchtask.setEnabled(true);
+                        break;
+
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + msg.what);
+                }
+                return false;
+            }
+        });
         threadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                threadPool.execute(new DoWorkThread());
-
+                threadArray.clear();
+                result.clear();
                 progressBar.setVisibility(View.VISIBLE);
-                p.setText(arraySize+getResources().getString(R.string.slash)+selectedProgress);
+                txtprogresshappened.setText(getResources().getString(R.string.zero)+getResources().getString(R.string.slash)+
+                        selectedProgress);
+                txtaverage.setText(getResources().getString(R.string.average)+" "+getResources().getString(R.string.avg_initial));
                 btn_asynchtask.setEnabled(false);
                 threadButton.setEnabled(false);
+                seekBar.setEnabled(false);
+
+                threadPool.execute(new DoWorkThread());
             }
         });
     }
 
     public class DoWorkThread implements Runnable{
         double randomNumber;
+        static final String NUMBER_KEY="NUMBER";
+        static final int STATUS_START = 0;
+        static final int STATUS_PROGRESS = 1;
+        static final int STATUS_END = 2;
+        static final String PROGRESS_KEY = "PROGRESS";
 
         @Override
         public void run() {
             HeavyWork heavyWork = new HeavyWork();
-            randomNumber = heavyWork.getNumber();
+            Bundle bundle = new Bundle();
 
-            Message message = new Message();
-            message.obj = randomNumber;
-            handler.sendMessage(message);
+            Message startMessage = new Message();
+            startMessage.what = STATUS_START;
+            bundle.putDouble(PROGRESS_KEY,selectedProgress);
+            startMessage.setData(bundle);
+            handler.sendMessage(startMessage);
+
+            for(int i=0;i<selectedProgress;i++) {
+                Message progressMessage = new Message();
+                randomNumber = heavyWork.getNumber();
+                bundle.putDouble(NUMBER_KEY, (double) randomNumber);
+                progressMessage.what = STATUS_PROGRESS;
+                progressMessage.setData(bundle);
+                handler.sendMessage(progressMessage);
+            }
+
+            Message endMessage = new Message();
+            endMessage.what = STATUS_END;
+            handler.sendMessage(endMessage);
         }
     }
 
     public class DoWorkAsynch extends AsyncTask<Integer, Double, ArrayList<Double>> {
         int i;
         Double average = 0.0;
+        Double sum = 0.0;
 
         @Override
         protected void onPreExecute() {
             progressBar.setVisibility(View.VISIBLE);
-            progressBar.setMax(selectedProgress-1);
-            txtprogresshappened.setText(0 +"/" +selectedProgress);
-            txtaverage.setText(getResources().getString(R.string.average)+"0.0");
+            progressBar.setMax(selectedProgress);
+            progressBar.setProgress(0);
+            txtprogresshappened.setText(getResources().getString(R.string.zero)+getResources().getString(R.string.slash)+
+                    selectedProgress);
+            txtaverage.setText(getResources().getString(R.string.average)+" "+getResources().getString(R.string.avg_initial));
             resultList =
                     new ArrayAdapter<Double>(MainActivity.this, android.R.layout.simple_list_item_1,android.R.id.text1, result);
             listviewresult.setAdapter(resultList);
+            sum = 0.0;
         }
 
         @Override
@@ -172,12 +224,12 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onProgressUpdate(Double... values) {
-            Double sum = 0.0;
+
             sum = sum + values [0];
             Integer progress =values[1].intValue();
             average = sum / (progress+1);
-            progressBar.setProgress(progress);
-            txtprogresshappened.setText(i +"/" +selectedProgress);
+            progressBar.setProgress(progress+1);
+            txtprogresshappened.setText(i +getResources().getString(R.string.slash) +selectedProgress);
             txtaverage.setText(getResources().getString(R.string.average)+" " +average);
             result.add(values[0]);
             listviewresult.setAdapter(resultList);
@@ -188,6 +240,7 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(ArrayList<Double> s) {
             threadButton.setEnabled(true);
             btn_asynchtask.setEnabled(true);
+            seekBar.setEnabled(true);
         }
     }
 }
